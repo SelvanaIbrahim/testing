0ba8c03e1776c32726e2c30b1b3309a4e8b820f2
SelvanaIbrahim <150904614+SelvanaIbrahim@users.noreply.github.com>
2023-11-26 10:53:36 +0200

    Update file.sh

--- file.sh
+++ file.sh
@@ -4,6 +4,5 @@
 #return 0;
 #}
 echo ">>>>>>>>>>>>>>  GOOD MORNING SALAVOTA   >>>>>>>>>>>>>>>"
-echo ">>>>>>>>>>>>>>  GOOD MORNING 2LP SALAVOTA   >>>>>>>>>>>>>>>"
-echo ">>>>>>>>>>>>>>  GOOD MORNING SALAVOTA   >>>>>>>>>>>>>>>"
+echo ">>>>>>>>>>>>>>  ?????  >>>>>>>>>>>>>>>"
 
